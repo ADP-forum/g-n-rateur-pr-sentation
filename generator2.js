8c8215040b0175521cd87fdf80fe5ef2429a820b
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2024-04-02 16:34:37 +0200

    Update generator2.js

--- generator2.js
+++ generator2.js
@@ -1,117 +1,28 @@
+// On affiche le générateur que sur le forum voulu
 document.addEventListener("DOMContentLoaded", function () {
   if (!window.location.href.includes("/post?f=6&mode=newtopic")) return;
+  document.querySelector("#formGenerateur").style.display = "block";
 
-  const targetFieldset = document.querySelector(".fields1");
-  if (!targetFieldset) return;
-
-  // Crée et insère le formulaire
-  insertForm(targetFieldset);
-  setupFormListener();
+  document.getElementById("generateBtn").addEventListener("click", getData);
 });
 
-function insertForm(target) {
-  const formHtml = createFormHtml();
-  const formContainer = document.createElement("div");
-  formContainer.innerHTML = formHtml;
-  target.insertBefore(formContainer, target.firstChild);
-}
+function getData() {
+  // Le container de tous les inputs
+  const generator = document.querySelector("#formGenerateur");
+  // On réccupère tous les inputs
+  const inputs = generator.querySelectorAll("input, select, textarea");
 
-function createFormHtml() {
-  return `<form id="formGenerateur" class="customForm">
-  <div class="form" style="padding: 20px;">
-  
-  <h2>Première partie</h2>
-  
-  <!-- champ du formulaire - champ pour texte court -->
-  <label for="triggerWarnings">Trigger Warnings :</label>
-  <input type="text" id="triggerWarnings" name="triggerWarnings">
-  <!-- fin du champ -->
-  
-  <label for="prenomNom">Prénom Nom :</label>
-  <input type="text" id="prenomNom" name="prenomNom">
-  
-  <label for="citation">Citation :</label>
-  <input type="text" id="citation" name="citation">
-  
-  <!-- champs du formulaire permettant l'ajout de l'url de l'avatar -->
-  <label for="avatarcélébrite">Url de l'avatar de la célébrité :</label>
-  <input type="text" id="avatarCelebrite" name="avatarCelebrite">
-  <!-- fin du champ -->
-  
-  <label for="age">Age :</label>
-  <input type="text" id="age" name="age">
-  
-  <label for="dateNaissance">Date de Naissance :</label>
-  <input type="text" id="dateNaissance" name="dateNaissance">
-  
-  <label for="lieuNaissance">Lieu de Naissance :</label>
-  <input type="text" id="lieuNaissance" name="lieuNaissance">
-  
-  <label for="nationalite">Nationalité :</label>
-  <input type="text" id="nationalite" name="nationalite">
-  
-  <label for="occupation">Occupation :</label>
-  <input type="text" id="occupation" name="occupation">
-  
-  <label for="statutCivil">Statut Civil:</label>
-  <input type="text" id="statutCivil" name="statutCivil">
-  
-  <label for="caractere">Caractère :</label>
-  <input type="text" id="caractere" name="caractere">
-  
-  <label for="faceclaim">Faceclaim :</label>
-  <input type="text" id="faceclaim" name="faceclaim">
-  
-  <!-- champ du formulaire - menu déroulant permettant de sélectionner une proposition -->
-  <label for="groupe">Groupe :</label>
-  <select id="groupe" name="groupe">
-    <option value="">Choisir le groupe</option>
-    <option value="Groupe1">Groupe 1</option>
-    <option value="Groupe2">Groupe 2</option>
-    <option value="Groupe3">Groupe 3</option>
-    <option value="Groupe4">Groupe 4</option>
-  </select>
-  <!-- fin du champ -->
-  
-  <label for="credits">Crédits :</label>
-  <input type="text" id="credits" name="credits">
-  
-  <h2>Deuxième partie</h2>
-  
-  <!-- champ du formulaire - champ pour texte long -->
-  <label for="histoire">Histoire du personnage :</label>
-  <textarea id="histoire" name="histoire"></textarea>
-  <!-- fin du champ -->
-  
-  <h2>Troisième partie</h2>
-  
-  <label for="pseudo">Pseudo, prénom :</label>
-  <input type="text" id="pseudo" name="pseudo">
-  
-  <label for="pronoms">Pronoms :</label>
-  <input type="text" id="pronoms" name="pronoms">
-  
-  <label for="pays">Pays:</label>
-  <input type="text" id="pays" name="pays">
-  
-  <label for="typePersonnage">Type de personnage:</label>
-  <select id="typePersonnage" name="typePersonnage">
-    <option value="">Choisir le type de personnage</option>
-    <option value="inventé">Inventé</option>
-    <option value="éphémère">Ephémère</option>
-    <option value="pré-lien">Pré-lien</option>
-  </select>
-  
-  <label for="motFin">Le mot de la fin:</label>
-  <input type="text" id="motFin" name="motFin"><br><br>
-  
-  <!-- bouton permettant de générer le code -->
-  <button type="submit" id="generateBtn" style="cursor: pointer;">Générer</button>
-  
-  <!-- fin du bouton -->
-  </div>
-  </form>
-  `;
+  const payload = {};
+  // Pour chaque input, on donne son nom en clé et sa valeur à la clé
+  Array.from(inputs).forEach((input) => {
+    // L'input doit avoir un nom (ça exclu les boutons par exemple)
+    if (input.name) {
+      payload[input.name] = input.value;
+    }
+  });
+
+  const generatedCode = generateCodeFromPayload(payload);
+  insertGeneratedCodeIntoEditor(generatedCode);
 }
 
 function setupFormListener() {
